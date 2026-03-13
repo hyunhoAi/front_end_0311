1e3709f8830b63b3472d3d086e4197140a318714
hyunhoAi <hyunho2212@gmail.congit config --global user.emailhyunho2212@gmail.con>
2026-03-13 12:40:42 +0900

    update

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,18 +1,18 @@
 document.addEventListListener("DOMContentLoaded",()=>{
     getData();
-})
+});
 
 //Spring Boot 서버에 요청하는 함수
 const getData = async()=> {
     
     // 로컬 테스트용
-    const API_URL = "http://localhost:8081/";
+    const API_URL = "http://localhost:8081/api/board/list";
 
     // 배포용
     // const API_URL = "";
     let res = await fetch(API_URL);
-    let data = await res.text();
+    let data = await res.json();
 
-    alert(data);
+    console.log(data);
 
 }
